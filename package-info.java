fad260aec542af7574e73407df3eba3bb03ff9eb
DrummerMC <drummermods@icloud.com>
2014-05-31 18:30:22 +0200

    Added EnergyGrid

--- package-info.java
+++ package-info.java
@@ -6,5 +6,5 @@
  *
  */
 @API(apiVersion = "0.1", owner = "DrummerMC", provides = "ae2addons|API")
-package DrummerMC.AE2_Addons.API;
+package DrummerMC.AE2_Addons.Api;
 import cpw.mods.fml.common.API;
